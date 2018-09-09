d1f6f99e34cca84e4648cb473647dc7c0b2c6d1e
Sam Spilsbury <smspillaz@gmail.com>
2018-09-09 23:46:27 +0300

    sql: Clarify that the question applies to the query in join-uneven.sql

--- sql/join-uneven.sql
+++ sql/join-uneven.sql
@@ -1,6 +1,5 @@
 -- Say that there are around five rows in the Type table and two
--- billion rows in the Record table. Why does in some cases will this query
--- run so slowly?
+-- billion rows in the Record table.
 
 CREATE TEMP TABLE Record (
     ID INT PRIMARY KEY,
@@ -11,11 +10,13 @@ CREATE TEMP TABLE Type (
     ID INT PRIMARY KEY
 );
 
+-- We create an index here which should speed things up....
 CREATE INDEX RecordByRecordType ON Record (RecordTypeID, ID);
 
 -- .... Data is inserted here, in a random order but with a
 -- surjective relationship between Type and Record.
 
+-- Question: Why in some cases will this query run so slowly?
 SELECT Record.ID
 FROM Record
 JOIN Type ON Type.ID = Record.RecordTypeID;
